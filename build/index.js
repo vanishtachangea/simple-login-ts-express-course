425fbd9dbbca4eef94936df08b13744fbc5fe447
Vanishta Changea <vanishta.changea@gmail.com>
2020-08-30 21:50:15 +0200

    Typescript in Express using type annotations

---
+++ build/index.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var loginRoute_1 = require("./routes/loginRoute");
+var body_parser_1 = __importDefault(require("body-parser"));
+var cookie_session_1 = __importDefault(require("cookie-session"));
+var app = express_1.default();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(cookie_session_1.default({ keys: ['log'] }));
+app.use(loginRoute_1.router);
+app.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import {router} from './routes/loginRoute';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieSession({keys:['log']}));
+app.use(router);
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000');
+})
