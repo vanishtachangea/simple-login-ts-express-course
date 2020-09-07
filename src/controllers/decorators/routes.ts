ff902e0f266dcc2c3d9669c314d0b2e8930cafcb
Vanishta Changea <vanishta.changea@gmail.com>
2020-09-07 13:12:50 +0200

    dfdsRevert "Property Descriptors for Type Checking"
    This reverts commit 638256292b16314fa29effff4bc1641662344323.

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,14 +1,10 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
-import { RequestHandler } from 'express';
 
-interface RouteHandlerDescriptor extends PropertyDescriptor{
-    value?:RequestHandler;
-} 
 function routeBinder(method: string) {
     return function (path: string) {
-        return function (target: any, key: string, desc: RouteHandlerDescriptor) {
+        return function (target: any, key: string, desc: PropertyDescriptor) {
             Reflect.defineMetadata(MetadataKeys.path, path, target, key);
             Reflect.defineMetadata(MetadataKeys.method, method, target, key);
         }
